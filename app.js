44a0f910de25e69665bd3bdd91c5878a903ff952
浦郷遼太郎 <jx16041@ns.kogakuin.ac.jp>
2018-02-17 02:12:46 +0900

    データベースのテーブル変更(見えない) 新しいjsファイルにsql関連は移行予定 mysqlが止まったのでここまで

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ server.listen(port, function(){
 //接続
 server.on("request",getdata);
 
+//mysqlからのデータ取得したい
+var mysql = require('mysql');
+let dbConfig = {
+    host: 'localhost',
+    database: 'kakeibo_db',
+    user: 'kakeibo',
+    password: 'kakeibokey'
+};
+let connection = mysql.createConnection(dbConfig);
+connection.connect();
+
 
 function getdata(req,res){
     //console.log(req);
@@ -23,6 +34,9 @@ function getdata(req,res){
     }else if(req.url == "/sub/index.js"){
         res.writeHead(200,{"Content-Type": "text/javascript"});
         Sfile("./sub/index.js",res);
+    }else if(req.url == "/sub/zougen.js"){
+        res.writeHead(200,{"Content-Type": "text/javascript"});
+        Sfile("./sub/zougen.js",res);
     }
 }
 
@@ -34,6 +48,8 @@ function Sfile(path,res){
         }
         if(path == "./sub/index.js"){
             sqladd(path,res,data);
+        }else if(path == "./sub/zougen.js"){
+            zougenadd(path,res,data);
         }else{
             res.end(data);
         }
@@ -42,31 +58,52 @@ function Sfile(path,res){
 }
 
 function sqladd(path,res,data){
-    //mysqlからのデータ取得したい
-    var mysql = require('mysql');
-    let dbConfig = {
-        host: 'localhost',
-        database: 'kakeibo_db',
-        user: 'kakeibo',
-        password: 'kakeibokey'
-    };
-    let connection = mysql.createConnection(dbConfig);
-    connection.connect();
-    var query = connection.query('select name,kane from zaisan;', function (err, results) {
+    connection.query('select name,kane from zaisan;', function (err, results) {
         //console.log('--- results ---');
         //console.log(results);
-        let objname;
-        let objnum;
 
         res.write ("\n let name =[]; \n let num =[];");
         for(let lop=0; lop < results.length ;lop++){
-            objname = "\n name[" + lop + "] = \"" + results[lop].name + "\";";
-            objnum = "\n num[" + lop + "] = " + results[lop].kane + ";";
-            console.log(objname);
-            console.log(objnum);
             
-            res.write(objname);
-            res.write(objnum);
+            res.write("\n name[" + lop + "] = \"" + results[lop].name + "\";");
+            res.write("\n num[" + lop + "] = " + results[lop].kane + ";");
+        }
+        res.write(data);
+        res.end();
+    });
+}
+/*
+id int AUTOINCREMENT　　主キー
+bunrui varchar 8
+basyo varchar 32
+kane int
+syurui varchar 32
+komento varchar 255;
+time date
+*/
+
+
+
+//途中！！！
+function zougenadd(path,res,data){
+    connection.query('select * from zougen;', function (err, results) {
+        //console.log('--- results ---');
+        //console.log(results);
+        res.write ("\n let id =[]; \n let bunrui =[];\n let basyo =[];\n let kane =[];");
+        res.write ("\n let syurui =[]; \n let komento =[];\n let time =[];");
+        for(let lop=0; lop < results.length ;lop++){
+            res.write("\n id[" + lop + "] = \"" + results[lop].id + "\";");
+            res.write("\n bunrui[" + lop + "] = \"" + results[lop].bunrui + "\";");
+            res.write("\n basyo[" + lop + "] = \"" + results[lop].basyo + "\";");
+            res.write("\n kane[" + lop + "] = \"" + results[lop].kane + "\";");
+            res.write("\n syurui[" + lop + "] = \"" + results[lop].syurui + "\";");
+            if(results[lop].komento == "null"){
+                res.write("\n komento[" + lop + "] = \" \";");
+            }else{
+                res.write("\n komento[" + lop + "] = \"" + results[lop].komento + "\";");
+            }
+            console.log(results[lop].time);
+            res.write("\n time[" + lop + "] = \"" + results[lop].time + "\";");
         }
         res.write(data);
         res.end();
